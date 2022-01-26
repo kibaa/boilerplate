685a49597364ca2e5a51cc70ae6a4722961ac139
kibaa <yuba24@naver.com>
2022-01-26 16:09:47 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+const { User } = require('./models/User');
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+//appication/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://Yuna:abc1234@boilerplate.7eygu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(() => console.log('MongoDB Connected...')).catch(err => console.log(err))
+mongoose.connect(config.mongoURI).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err))
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!~안녕하세요~~')
+  res.send('Hello World!~안녕하세요~~333 New')
+})
+app.post('/register', (req, res) => {
+  //회원가입할 때 필요한 정보를 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다
+  const user = new User(req.body)
+  user.save((err, userInfo) => {
+    //에러
+    if (err) return res.json({ success: false, err })
+    //성공
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
